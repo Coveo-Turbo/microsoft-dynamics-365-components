0edb6fdaa21c1f42a7f5c085a1a43dc441e7e21d
Louis-Guillaume Carrier-Bedard <lgcarrier@wks-000646.lan>
2020-11-10 10:22:20 -0500

    feat: update default template for ResultActionsTakeNote

--- src/ui/ResultActionsTakeNote/DefaultResultActionsTakeNoteTemplate.ts
+++ src/ui/ResultActionsTakeNote/DefaultResultActionsTakeNoteTemplate.ts
@@ -2,6 +2,6 @@ import { IQueryResult, Template } from "coveo-search-ui";
 
 export class DefaultResultActionsTakeNoteTemplate extends Template {
     public instantiateToString(queryResult?: IQueryResult): string {
-        return `<div><span>The following item has been marked as relevant: </span><span class=\"CoveoFieldValue\" data-field=\"@clickuri\"></span></div>`;
+        return `<div><span>Marked as relevant: </span><span class=\"CoveoResultLink\"></span></div>`;
     }
 }
